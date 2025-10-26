828dc4c483f1119de22eaf1e387bb0cbe363f987
zericiia <karimbrahimi102@gmail.com>
2025-10-26 05:33:05 +0000

    split a header into a component nd add Links

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,37 +1,11 @@
 import "./OrdersPage.css";
-import "./header.css";
+import { Header } from "../componenets/Header";
 export function OrdersPage() {
   return (
     <>
       <title>Orders</title>
-      <div className="header">
-        <div className="left-section">
-          <a href="/" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </a>
-        </div>
 
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="orders">
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="checkout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
+      <Header />
 
       <div className="orders-page">
         <div className="page-title">Your Orders</div>
--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,38 +1,10 @@
 import "./TrackingPage.css";
-import "./header.css";
+import { Header } from "../componenets/Header";
 export function TrackingPage() {
   return (
     <>
       <title>Tracking</title>
-      <div className="header">
-        <div className="left-section">
-          <a href="/" className="header-link">
-            <img className="logo" src="images/logo-white.png" />
-            <img className="mobile-logo" src="images/mobile-logo-white.png" />
-          </a>
-        </div>
-
-        <div className="middle-section">
-          <input className="search-bar" type="text" placeholder="Search" />
-
-          <button className="search-button">
-            <img className="search-icon" src="images/icons/search-icon.png" />
-          </button>
-        </div>
-
-        <div className="right-section">
-          <a className="orders-link header-link" href="Orders">
-            <span className="orders-text">Orders</span>
-          </a>
-
-          <a className="cart-link header-link" href="Orderscheckout">
-            <img className="cart-icon" src="images/icons/cart-icon.png" />
-            <div className="cart-quantity">3</div>
-            <div className="cart-text">Cart</div>
-          </a>
-        </div>
-      </div>
-
+      <Header />
       <div className="tracking-page">
         <div className="order-tracking">
           <a className="back-to-orders-link link-primary" href="Orders">
